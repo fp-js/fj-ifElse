773c964d86f56ec71f3fb4175b058356df0e6532
stoeffel <schtoeffel@gmail.com>
2015-02-04 14:26:10 +0100

    return a function from ifElse

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ var curry3 = require("fj-curry").curry3;
 
 
 function _ifElse(pred, then, otherwise) {
-  if (pred() === true) {
-    return then();
-  } else {
-    return otherwise();
-  }
+  return function () {
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    if (pred.apply(null, args) === true) {
+      return then.apply(null, args);
+    } else {
+      return otherwise.apply(null, args);
+    }
+  };
 }
 
 var ifElse = curry3(_ifElse);
